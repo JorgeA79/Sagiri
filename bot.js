300c0dfa678e14a712c76d3382c8f4150d4853fb
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-22 17:09:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,43 +2,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready',() => {
-	//Edit after game so u can add wut game bot is playing between "" xd
-	   client.user.setPresence({game: {name: "", type: 0}});
+	   client.user.setPresence({game: {name: "to Draw | s!help", type: 0}});
 });
 
-var prefix = "m."
+var prefix = "s!"
 
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'ping')) {
 	
-		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
+		 message.channel.send(`:ping_pong: Pong! \nTime taken: \`${Date.now() - message.createdTimestamp} ms\``);
 	}
 });
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'hello')) {
-	
-		 message.channel.send(`Hello ${message.author.username}! :wave:`);
-		
-	}
-});
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'xd')) {
-	
-		 message.channel.send(`XD`);
-	}
-});
-//Copy and paste from here to add a command, replace mimikkip with ur command and here goes ur reply to wut bot is going to say
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'mimikkip')) {
-	
-		 message.channel.send(`here goes ur reply`);
-		
-	}
-});
+
 //Important
 client.login(process.env.BOT_TOKEN);
 
