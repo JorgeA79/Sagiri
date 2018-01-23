958970334da19eced2c658d3017267bc08ed7257
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-22 20:08:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 	if (message.content.startsWith(prefix + 'help')) {
 		
 	
-	message.author.send(`${message.author.username} you need a little help with the commands, there you go:\n **-j.hello**\n **-j.dbs**\n **-j.ssj**\n **-j.ssgss**\n **-j.dex help**\n **-j.xd**\n **-j.fact**\n **-j.wait**\n **-j.ping**\n **-j.join**\n **-j.invite**`);
+	message.author.send(`\```${message.author.username} you need a little help with the commands, there you go:\n **-j.hello**\n **-j.dbs**\n **-j.ssj**\n **-j.ssgss**\n **-j.dex help**\n **-j.xd**\n **-j.fact**\n **-j.wait**\n **-j.ping**\n **-j.join**\n **-j.invite**\````);
 	message.channel.send(`${message.author.username} i sent you a message that will help you a bit :mailbox_with_mail:`)
 		
 	}
@@ -53,17 +53,6 @@ client.on('message', message => {
 	}
 		
 });
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'flip')) {
-		
-	
-		
-    		message.channel.send(`The coin landed on heads`);
-
-	}
-});
-
 //Important
 client.login(process.env.BOT_TOKEN);
 
