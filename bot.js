4b20e2a5db04f5a1375e72d30c002d4ffe6b04e3
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-22 20:25:15 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,21 +34,11 @@ client.on('message', message => {
 	 	var result = Math.floor((Math.random() * 2) + 1);
     	if (result == 1) {
     		
-		const embed = new Discord.RichEmbed()
- 		 .setTitle(">Head<")
-  		 .setColor(0x7AFFA8)
- 		 .setTimestamp()
- 		 .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif")
-  		 message.channel.send({embed});	 
+		
 	}
     	} else if (result == 2) {
     			
-		const embed = new Discord.RichEmbed()
- 		 .setTitle(">Tail<")
-  		 .setColor(0x7AFFA8)
- 		 .setTimestamp()
- 		 .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif")
-  		 message.channel.send({embed});	 
+		
 	}
     	}
 	
