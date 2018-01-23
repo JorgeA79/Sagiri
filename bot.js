992b174735b43c09f50faee00d4b68d18e3a2eaa
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-22 21:07:52 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,27 @@ client.on('ready',() => {
 });
 
 var prefix = "s!"
+var answers = 
+    			["It is certain",
+			"It is decidedly so",
+			"Without a doubt",
+			"Yes, definitely",
+			"You may rely on it",
+			"As I see it, yes",
+			"Most likely",
+			"Outlook good",
+			"Yes",
+			"Signs point to yes",
+			"Reply hazy try again",
+			"Ask again later",
+			"Better not tell you now",
+			"Cannot predict now",
+			"Concentrate and ask again",
+			"Don't count on it",
+			"My reply is no",
+			"My sources say no",
+			"Outlook not so good",
+			"Very doubtful"];
 //Normal Commands
 client.on('message', message => {
 	if (message.author === client.user) return;
@@ -115,6 +136,13 @@ client.on('message', message => {
 		 message.channel.send(`You got me <:anime:405162482370347019>`);
     	}
 		
+	}
+});
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + '8ball')) {
+	var r8ballAnswer = answers[Math.floor(Math.random() * answers.length)];
+		message.channel.send(r8ballAnswer);
 	}
 });
 //Important
