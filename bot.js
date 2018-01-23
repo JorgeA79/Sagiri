a1b476296e5089077ca1791f6a75d400b0177e7d
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-23 17:01:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const fs = require("fs");
+let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
 
 client.on('ready',() => {
 	   client.user.setPresence({game: {name: "to Draw | s!help", type: 0}});
@@ -221,6 +222,32 @@ client.on('message', message => {
 		var dbsfactAnswer = dbsfacts[Math.floor(Math.random() * dbsfacts.length)];
 		message.channel.send(dbsfactAnswer);
 		}
+});
+client.on("message", message => {
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+
+  if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+    level: 0
+  };
+  let userData = points[message.author.id];
+  userData.points++;
+
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (curLevel > userData.level) {
+    // Level up!
+    userData.level = curLevel;
+    message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
+  }
+
+  if (message.content.startsWith(prefix + "level")) {
+    message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  });
+
 });
 //Important
 client.login(process.env.BOT_TOKEN);
