59f6da0f44800622b21cc369ffe68ec4bfffbb59
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-22 20:46:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,14 +60,22 @@ client.on('message', message => {
 		
 	 	var result = Math.floor((Math.random() * 2) + 1);
     	if (result == 1) {
-    		
+    		const embed = new Discord.RichEmbed()
+
+  .setTitle(">Head<")
+
+  .setColor(0x7AFFA8)
+  .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif")
+
+
+  message.channel.send({embed});
     	} else if (result == 2) {
     		const embed = new Discord.RichEmbed()
 
-  .setTitle("AAAAH KAAAAIOKEEN")
+  .setTitle(">Tail<")
 
-  .setColor(0x01DFD7)
-  .setImage("https://media.giphy.com/media/3osBLqfVbQI0fx0oLe/giphy.gif")
+  .setColor(0x7AFFA8)
+  .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif")
 
 
   message.channel.send({embed});
