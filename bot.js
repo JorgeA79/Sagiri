1ecb33acb5d7084429a714220ffaf30f12ae967a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-24 17:56:56 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const music = require('discord.js-music-v11');
+const yt = require('ytdl-core');
+
 const fs = require("fs");
 
 let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
@@ -279,7 +280,32 @@ experience = userData.points
 
 });
 
-music(client);
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'play')) {
+		
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel){
+      return message.channel.sendMessage(":x: You must be in a voice channel first!");
+    }
+    voiceChannel.join()
+    .then(connection => {
+      let stream = yt(args.join(" "), {audioonly: true});
+      yt.getInfo(args.join(" "), function(err, info) {
+      const title = info.title
+      console.log(`${message.author.username}, Queued the song '${title}.'`)
+      message.channel.sendMessage(`Now playing \`${title}\``)
+      })
+      const dispatcher = connection.playStream(stream);
+      dispatcher.on('end', () => {
+         voiceChannel.leave();
+       }).catch(e =>{
+         console.error(e);
+       });
+    })
+  }
+});
+
 //Important
 client.login(process.env.BOT_TOKEN);
 
