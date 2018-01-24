b144d1ec61f3536a59b296a2650c033acc4ecab7
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-24 16:19:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -277,6 +277,20 @@ experience = userData.points
   });
 
 });
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'awaitest')) {
+		
+ message.author.send("See or Change?");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content == "See") {
+                message.channel.send("You Want To See Someones Spec OK!");
+            } else if (message.content == "Change") {
+                message.channel.send("You Want To Change Your Spec OK!");
+            }
+        });
 
 //Important
 client.login(process.env.BOT_TOKEN);
