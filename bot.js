250cad54d090f012b45b011ce2eb532de7a4ba69
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-24 15:20:41 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,34 @@ client.on('message', message => {
 	}
 		
 });
+	client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'profile')) {
 
+		var username = message.author.username
+		var avatar = message.author.avatarURL
+		const embed = new Discord.RichEmbed()
+
+  .setAuthor(username, avatar)
+  .setColor(0x7AFFA8)
+  
+  .setThumbnail(avatar)
+
+
+
+  .addField(":star:Levels", levels + " (" + experience + " xp/ " + levelsequ + " xp for level " + nextlevel + ")", true)
+
+  .addField(":dollar:Credits", "$" + balance , true)
+
+  .addField(":yellow_heart:Reputation",
+    reputation)		
+  message.channel.send({embed});
+		
+		 
+	}
+		
+});
+	
 //MiniGames
 client.on('message', message => {
 	if (message.author === client.user) return;
@@ -239,34 +266,7 @@ client.on("message", message => {
 levels = userData.level
 experience = userData.points
 	
-	client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'profile')) {
 
-		var username = message.author.username
-		var avatar = message.author.avatarURL
-		const embed = new Discord.RichEmbed()
-
-  .setAuthor(username, avatar)
-  .setColor(0x7AFFA8)
-  
-  .setThumbnail(avatar)
-
-
-
-  .addField(":star:Levels", userData.level + " (" + userData.points + " xp/ " + (32*(userData.level)+32); + " xp for level " + userData.level + 1 + ")", true)
-
-  .addField(":dollar:Credits", "$" + balance , true)
-
-  .addField(":yellow_heart:Reputation",
-    reputation)		
-  message.channel.send({embed});
-		
-		 
-	}
-		
-});
-	
   if (message.content.startsWith(prefix + "level")) {
     message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
 	  
