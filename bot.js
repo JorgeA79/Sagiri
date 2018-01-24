ad3b9ce19c8b1ab3cb886245564a6cd73fa69432
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-23 20:01:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const sql = require("sqlite");
-sql.open("./score.sqlite");
 const Enmap = require('enmap');
-
+const EnmapLevel = require('enmap-level');
 
 
 client.on('ready',() => {
