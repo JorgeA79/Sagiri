eeccabe296fcc3cc921e08bafc7cf2f9cacca0ea
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-24 16:21:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -281,14 +281,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'awaitest')) {
 		
- message.author.send("See or Change?");
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-        console.log(collector)
-        collector.on('collect', message => {
-            if (message.content == "See") {
-                message.channel.send("You Want To See Someones Spec OK!");
-            } else if (message.content == "Change") {
-                message.channel.send("You Want To Change Your Spec OK!");
+
             }
         });
 
