a80d1e9f42d48c9e516d6c00d9ce695cce7549fb
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-24 00:44:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,10 +71,9 @@ client.on('message', message => {
 .setThumbnail("https://cdn.discordapp.com/attachments/405118984451653633/405149226604167179/91380.png")
 
   .addField("Main Commands",
-    "\`help\`,\`ping\`,\`invite\,\`profile \`. ")
+    "\`help\`,\`ping\`,\`invite\,\`profile\`")
 
   .addField("Fun Commands", "\`8ball\`, \`flip\` \`head\` or \`tail\`", true)
-	.addBlankField(true)
   .addField("Anime Commands", "\`fact pokemon\`, \`fact db\`", true)
 
   message.author.send({embed});
