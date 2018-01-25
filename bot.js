26416a373779ba483c78e4144b05c7d65b6b294d
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-24 20:22:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -289,15 +289,16 @@ client.on('message', message => {
 	
 		  const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel){
-      return message.channel.sendMessage(":x: You must be in a voice channel first!");
+      return message.channel.sendMessage(":x: You are not in a voice channel!!");
     }
+	message.channel.sendMessage(":white_check_mark: **Connected!**");
     voiceChannel.join()
     .then(connection => {
+	const args = message.content.split(" ").slice(1);
       let stream = yt(args.join(" "), {audioonly: true});
       yt.getInfo(args.join(" "), function(err, info) {
       const title = info.title
-      console.log(`${message.author.username}, Queued the song '${title}.'`)
-      message.channel.sendMessage(`Now playing \`${title}\``)
+	  message.channel.sendMessage(`Now playing \`${title}\``)
       })
       const dispatcher = connection.playStream(stream);
       dispatcher.on('end', () => {
