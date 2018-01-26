83fa823c3ac9580a493b1bee731306333ddfd3c8
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-25 19:25:34 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -191,7 +191,7 @@ client.on('message', message => {
 	if (message.content.startsWith(prefix + 'hug')) {
  let member = message.mentions.members.first();
 		var selecthugGif = hugifs[Math.floor(Math.random() * hugifs.length)];
-		  message.channel.send(`${message.author.username} hugged member`);
+		  message.channel.send(`**${message.author.username}** hugged **${member.username}**`);
 		const embed = new Discord.RichEmbed()
 
   .setImage(selecthugGif)
