e0de759eeb655637f42f0cd4edf387464b68f3f7
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-27 03:45:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -270,12 +270,13 @@ client.on('message', message => {
 		
 		
 		  let dexmon = args.slice(1).join(" ");
+		var dextext = dexmon;
 		
 		 if(!dexmon) 
 		return message.reply("Try saying the pokemon");
 		
 		
-		console.log(pokedex.pokemon(dexmon);
+		console.log( pokedex.pokemon(dextext) );
 		
 
   	
