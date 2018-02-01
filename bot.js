a8e381bfd62c408b213ef8907016ceeaa8376c68
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-02-01 17:28:14 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -345,7 +345,7 @@ client.on('message', message => {
 
       let mon = args;
 		
-		var dexmon = mirrendex(args);
+		var dexmon = mirrendex.message.content.slice(prefix.length).trim().split(/ +/g);
 		
 		const embed = new Discord.RichEmbed()
   .setTitle(dexmon.name)
