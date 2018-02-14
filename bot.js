6c7edbab39c9ae72554a7aa9386cf383dec1f61a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-02-13 19:11:04 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,6 +122,11 @@ var dbsfacts = [
   "Did you know that The fight between Goku and Frieza took up a total of three and a half hours of screen time, making it the longest fight in anime history.",
   "Did you know that Goku's mother is a Saiyan known as Gine."
 ]
+var hentai = [
+ "https://danbooru.donmai.us/data/__original_drawn_by_mike_inel__f92c3b06d6d89b0dda866cdad4a49edf.gif"
+	"https://files.yande.re/image/bdb701e0daea5e54a6cfd98b06102efb/yande.re%20432843%20bondage%20breast_hold%20fate_grand_order%20mash_kyrielight%20naked%20nipples%20pussy_juice%20remana%20tentacles%20vibrator.jpg",
+	"https://konachan.com/image/ca216778f744b40204e3e0fcb514f57e/Konachan.com%20-%20260111%20anal%20anus%20aqua_eyes%20aqua_hair%20ass%20ass_grab%20bed%20bell%20bow%20breasts%20censored%20maid%20nipples%20no_bra%20panties%20pussy%20sayika%20short_hair%20underwear%20vibrator.jpg"
+]
 var pokefacts = [
  "Did you know that In Pokemon stadium, it was revealed how Doduo can learn the move fly. Apparently it just runs really fast and the running motion gives it the power of flight! It just floats there, running in place...",
   "Did you know that Genesect is a very futuristic Pokemon, and Kabutops is an ancient fossil. However, there are rumors that Genesect is actually a Kabutops that's been modified. Although the two don't share any of the same types and use barely any of the same attacks, there is a definite resemblence, especially when you compare Genesect's head to Kabuto.",
@@ -537,7 +542,13 @@ client.on('message', message => {
 		message.channel.send(dbsfactAnswer);
 		}
 });
-
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'hentai')) {
+		var hentfactAnswer = hentai[Math.floor(Math.random() * hentai.length)];
+		message.channel.send(hentfactAnswer);
+		}
+});
 //Music System
 client.on('message', message => {
 	if (message.author === client.user) return;
